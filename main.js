f783f88a4b580adbca03771ee7e90fdc2d4478b4
arushdeshpande <73320706+arushdeshpande@users.noreply.github.com>
2021-06-24 11:51:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -30,3 +30,50 @@ fabric.Image.fromURL(get_image,function(Img){
     canvas.add(block_image_obj);
 });
 }
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e) {
+    keyPressed = e.keycode;
+    console.log(keyPressed == '80');
+    if(e.shiftKey == true && keyPressed == '80')
+    {
+        console.log("p and shift pressed together");
+        block_image_width = block_image_width + 10;
+        block_image_height = block_image_height + 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if(e.shiftKey && keyPressed == '77')
+    console.log("m and shift pressed together");
+    block_image_width = block_image_width - 10;
+    block_image_height = block_image_height - 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+}
+if(keyPressed == '38'){
+    up();
+    console.log("up");
+}
+if(keyPressed == '40'){
+    down();
+    console.log("down");
+}
+if(keyPressed == '37'){
+    left();
+    console.log("left");
+}
+if(keyPressed == '39'){
+    right();
+    console.log("right");
+}
+if(keyPressed == '87'){
+    new_image(wall.jpg)
+    console.log("w")
+}
+if(keyPressed == '71'){
+    new_image(ground.png)
+    console.log("g")
+}
+if(keyPressed == '76'){
+    new_image(light_green.png);
+    console.log("l");
+}
